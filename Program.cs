e305a9b894ef3378d36bb4af299c6cea1f3720ef
HibbaHindawi <hibba04@hotmail.se>
2022-02-15 15:58:26 +0100

    Mergesort

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+namespace Mergesort
+{
+    class MergeSort {
+        void merge(int[] arr, int l, int m, int r){
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+            for (i = 0; i < n1; ++i){
+                L[i] = arr[l + i];
+            }
+            for (j = 0; j < n2; ++j){
+                R[j] = arr[m + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2) {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        void sort(int[] arr, int l, int r){
+            if (l < r) {
+                int m = l+ (r-l)/2;
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+                merge(arr, l, m, r);
+            }
+        }
+        static void printArray(int[] arr){
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i){
+                Console.Write(arr[i] + " ");
+                }
+            Console.WriteLine();
+        }
+      public static void Main(String[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int[] arr = new int[1000];
+            Random random = new Random();
+            for(int i = 0; i < 1000; i++){
+                arr[i] = random.Next(1, 101); //1 - 100, including 100
+            }            
+            MergeSort ob = new MergeSort();
+            ob.sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+            printArray(arr);
+            watch.Stop();
+            long ts = watch.ElapsedMilliseconds;
+            Console.WriteLine("RunTime " + ts);
+        }
+    }
+}
